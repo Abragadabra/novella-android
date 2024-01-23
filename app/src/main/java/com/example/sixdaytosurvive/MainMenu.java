6343f60fcb003d3e0c5e3166a1c4a29fef173127
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2024-01-23 20:12:39 +0500

    Закрытие приложения по нажатию на кнопку

--- app/src/main/java/com/example/sixdaytosurvive/MainMenu.java
+++ app/src/main/java/com/example/sixdaytosurvive/MainMenu.java
@@ -3,6 +3,7 @@ package com.example.sixdaytosurvive;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -11,6 +12,12 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_name);
 
+        // Задача размеров приложения на весь экран
         getWindow().setFlags(512, 512);
+    }
+
+    public void closeGame(View view) {
+        // Закрытие игры
+        finishAndRemoveTask();
     }
 }
