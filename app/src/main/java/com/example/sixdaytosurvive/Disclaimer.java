99b4f4230f912847ee648593c50357787a92980d
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2024-01-31 22:16:39 +0500

    первоначальная настройка активити первого дня
    - из дисклеймера переход на новую активити (раньше вела на гослинга)
    - картинка гардероба

--- app/src/main/java/com/example/sixdaytosurvive/Disclaimer.java
+++ app/src/main/java/com/example/sixdaytosurvive/Disclaimer.java
@@ -43,7 +43,7 @@ public class Disclaimer extends AppCompatActivity {
 
     public void startNewGame(View view) {
         // Новый intent для открытия главного меню
-        Intent intent = new Intent(Disclaimer.this, Gosling.class);
+        Intent intent = new Intent(Disclaimer.this, FirstDay.class);
 
         // Открываем новое активити
         Disclaimer.this.startActivity(intent);
