177da93778ab24d301474f9b2f3dfb9dea352444
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2024-02-13 12:49:53 +0500

    Первый монолог
    монолог гг, когда он просыпается

--- app/src/main/java/com/example/sixdaytosurvive/Dialogues.java
+++ app/src/main/java/com/example/sixdaytosurvive/Dialogues.java
@@ -2,5 +2,7 @@ package com.example.sixdaytosurvive;
 
 public class Dialogues {
     // день 1
-    public static String day1_dialog1 = "Мы короче опаздываем в гардероб и мы скоро получим по жопу";
+    public static String day1_dialog1 = "Каждый день мне так сложно вставать, зачем я хожу туда? Для чего я каждый раз просыпаюсь и иду в этот тех... Это одеяло такое тяжолое... Оно придавило меня...";
+    public static String day1_dialog2 = "";
+    
 }
