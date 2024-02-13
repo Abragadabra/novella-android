5ad6c0d6f61f3cb5e0e1b2300e81b590a06e16b8
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2024-02-13 12:51:26 +0500

    второй монолог
    второй монолог, в котором мы раскрываем персонажа Кастрюлевой

--- app/src/main/java/com/example/sixdaytosurvive/Dialogues.java
+++ app/src/main/java/com/example/sixdaytosurvive/Dialogues.java
@@ -3,6 +3,6 @@ package com.example.sixdaytosurvive;
 public class Dialogues {
     // день 1
     public static String day1_dialog1 = "Каждый день мне так сложно вставать, зачем я хожу туда? Для чего я каждый раз просыпаюсь и иду в этот тех... Это одеяло такое тяжолое... Оно придавило меня...";
-    public static String day1_dialog2 = "";
+    public static String day1_dialog2 = "Я проспал! Нужно срочно спешить в тех, сегодня же пара Кострюлевой! Главное не опоздать...";
     
 }
