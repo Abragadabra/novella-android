107c8fc92840f5f530f6bae53611f91f96e55571
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2024-02-13 13:08:29 +0500

    Полный выбор с курткой и первая пара

--- app/src/main/java/com/example/sixdaytosurvive/Dialogues.java
+++ app/src/main/java/com/example/sixdaytosurvive/Dialogues.java
@@ -6,9 +6,15 @@ public class Dialogues {
     public static String day1_dialog2 = "Я проспал! Нужно срочно спешить в тех, сегодня же пара Кострюлевой! Главное не опоздать...";
     public static String day1_warderobe1 = "Снова большая очередь в гардероб! Что мне делать? Как мне успеть на пару? Может попытаться сложить куртку в рюкзак?";
     public static String day1_warderobe_variant1 = "Я сложил куртку в рюкзак... Это было очень сложно, но теперь я успеваю добежать до пары!";
-    public static String day1_warderobe_variant2 = "Очередь доложна была пройти быстро... Как же я ошибался... Я уже опоздал... Что же со мной будет?!";
-    public static String day1_warderobe_variant2 = "Здравствуйте, извините за опоздание, можно войти?";
-    public static String day1_warderobe_variant2 = "Кастрюлева: Опять ты, сколько можно опаздывать? Иди за допуском!";
+    public static String day1_warderobe_variant2_1 = "Очередь доложна была пройти быстро... Как же я ошибался... Я уже опоздал... Что же со мной будет?!";
+    public static String day1_warderobe_variant2_2 = "Здравствуйте, извините за опоздание, можно войти?";
+    public static String day1_warderobe_variant2_3 = "Кастрюлева: Опять ты, сколько можно опаздывать? Иди за допуском!";
+    public static String day1_warderobe_variant2_4 = "По пути за Допуском я встретил Дырова...";
+    public static String day1_warderobe_variant2_5 = "Дыров: Дарова! (Мы очень сильно пожали друг другу руки) Ты тоже за допуском? Я просто играл всю ночь в Hogwar Legac... Зря... Но зато я доволен!";
+    public static String day1_warderobe_variant2_6 = "Получив допуск мы вместе пошли обратно к этой старой корге...";
+    public static String day1_class1_1 = "Зайдя в кабинет, я сел вместе со Степанидой. Она сидела одна, поэтому я решил сесть рядом...";
+    public static String day1_class1_2 = "Наискучнейший преподаватель, как можно вести пару настолько неумело и настолько профессионально скучно... Я засыпаю...";
+    public static String day1_class1_3 = "Я так и проспал всю эту пару, удивительно, но меня так и не разбудили... зато теперь обед, вкусновое!";
     
     
     
