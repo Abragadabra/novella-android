0c2797133ffd612dae1dda73099c18f4bb0a497e
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2024-03-03 00:26:28 +0500

    Вёрстка титров

--- app/src/main/java/com/example/sixdaytosurvive/Titres.java
+++ app/src/main/java/com/example/sixdaytosurvive/Titres.java
@@ -1,6 +1,7 @@
 package com.example.sixdaytosurvive;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,16 @@ public class Titres extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_titres);
 
+        // Скрытие UI элементов android
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
+        // Размеры приложения занимают весь экран
+        getWindow().setFlags(512, 512);
     }
 }
