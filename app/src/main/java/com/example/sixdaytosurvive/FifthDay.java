523edecc320c195e316110c00cba18ad4f3c5de6
Александр Минин <77795206+Abragadabra@users.noreply.github.com>
2024-03-02 18:17:37 +0500

    добавлен пустой шестой день

---
+++ app/src/main/java/com/example/sixdaytosurvive/FifthDay.java
@@ -0,0 +1,31 @@
+package com.example.sixdaytosurvive;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FifthDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth_day);
+
+        // Скрытие UI элементов android
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        // Размеры приложения занимают весь экран
+        getWindow().setFlags(512, 512);
+    }
+}
